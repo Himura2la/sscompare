85059e088a27cd28ee9cff73af158b1587ac1d0e
Himura Kazuto <glagol15@gmail.com>
2021-08-06 23:56:20 +0300

    add comparison function

--- main.py
+++ main.py
@@ -12,19 +12,26 @@ class SSTime(object):
         self.ss = ss
 
     def __str__(self):
-        ss = format(self.ss, ',.2f').replace(',', "'")
-        return f"{self.year}-{self.month}-{self.date} {ss}"
+        ss = format(self.ss, ",.2f").replace(",", "'")
+        return f'{self.year}-{self.month}-{self.date} {ss}'
 
     @classmethod
-    def make(cls, time: datetime):
+    def make(cls, std_time: datetime):
         # original idea: https://github.com/cosmolabs-ru/sssync/blob/master/main.py
-        dt = time.timetuple()
+        dt = std_time.timetuple()
         hour = dt.tm_hour - 2
         if hour < 0:
             hour = 24 + hour
-        ss = (hour * 3600 + dt.tm_min * 60 + dt.tm_sec) * (1000/864)
-        return cls(dt.tm_year-1, floor(dt.tm_yday / 73), dt.tm_yday % 73 - 1, ss)
+        return cls(year=dt.tm_year - 1,
+                   month=floor(dt.tm_yday / 73),
+                   date=dt.tm_yday % 73 - 1,
+                   ss=(hour * 3600 + dt.tm_min * 60 + dt.tm_sec) * (1000/864))
+
+
+def compare(std_time: datetime, now=False):
+    print(f"{std_time.isoformat(sep=' ', timespec='seconds')}\t{'*' if now else '|'}\t{SSTime.make(std_time)}")
 
 
 if __name__ == "__main__":
-    print(SSTime.make(datetime.utcnow()))
+
+    compare(datetime.utcnow(), True)

c8a032d5db9c8720e35edf290b7430bacd4f9c31
Himura Kazuto <glagol15@gmail.com>
2021-08-07 03:25:21 +0300

    Use extracted timestamp converter

--- convert_ss.js
+++ convert_ss.js
@@ -1,8 +1,7 @@
-const Ss = require('./simple-second/lib/ss.js')
+const ssLib = require('./simple-second/lib/ss.js')
 
 const input_timestamp = parseInt(process.argv[2])
-const ss_timestamp = Math.round((1000/864)*(new Date(input_timestamp)-2*60*60*1000)+((1492*365+477*366)*100000000)) // TODO: add to lib
-const ss = new Ss(ss_timestamp)
+const ss = ssLib.dateToSs(new Date(input_timestamp))
 const response = {
     'year': ss.getYear(),
     'month': ss.getMonth(),
